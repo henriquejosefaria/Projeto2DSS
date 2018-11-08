0853cfc2b82288c0393d8c5919430f9c3620df1c
RyzonPT <Filipe-Cunha1@hotmail.com>
2018-11-08 11:22:16 +0000

    ola

---
+++ InterfaceGrafica/src/interfacegrafica/InterfaceGrafica.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacegrafica;
+
+/**
+ *
+ * @author Filipe Universidade
+ */
+public class InterfaceGrafica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
