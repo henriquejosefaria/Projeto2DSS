c9ced53abae9997e55af57d3ce4c88d0bb7eb08a
André Peixoto <36737417+AndreGpx@users.noreply.github.com>
2018-12-23 20:03:44 +0000

    classes e métodos principais

---
+++ Codigo (com interface grafica incluida)/src/codigo/HashUsers.java
@@ -0,0 +1,21 @@
+
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author André
+ */
+public class HashUsers {
+    
+    private HashMap<String, Utilizador> utilizadores; //organizado pelos nomes
+    
+    public HashUsers(){
+        utilizadores = new HashMap<String, Utilizador>();
+    }
+}
