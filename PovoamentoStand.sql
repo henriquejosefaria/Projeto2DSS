c493c2b5323ff28e7b34b3e7a373a07e44c251e2
RyzonPT <Filipe-Cunha1@hotmail.com>
2018-12-28 22:47:28 +0000

    imagens tudo a dar

--- PovoamentoStand.sql
+++ PovoamentoStand.sql
@@ -1,31 +1,31 @@
 INSERT INTO componente
 	(Nome,Stock,Tipo,Preco,Descricao,Imagem)
 	VALUES
-		('Jantes Liga Leve',832,'Jante',434.32,'Rodam bem','/interfacegrafica/Img/Motor/motor3.jpg'),
-        ('Jantes Liga Pesada',454,'Jante',434.32,'Rodam bem pesadamente','/interfacegrafica/Img/Motor/motor3.jpg'),
-        ('Jantes Liga Media',232,'Jante',434.32,'Rodam bem as vezes','/interfacegrafica/Img/Motor/motor3.jpg'),
+		('Jantes Liga Leve',832,'Jante',434.32,'Rodam bem','/interfacegrafica/Img/Jantes/Jantes1.jpg'),
+        ('Jantes Liga Pesada',454,'Jante',434.32,'Rodam bem pesadamente','/interfacegrafica/Img/Jantes/Jantes2.jpg'),
+        ('Jantes Liga Media',232,'Jante',434.32,'Rodam bem as vezes','/interfacegrafica/Img/Jantes/Jantes3.jpg'),
 	    
         ('Motor 4 CILINDROS TURBO',324,'Motor',4356.32,'Puxa bem','/interfacegrafica/Img/Motor/motor1.jpg'),
         ('Motor 8 CILINDROS TURBO PLUS',34,'Motor',12344.32,'Puxa que nem é bom','/interfacegrafica/Img/Motor/motor2.jpg'),
         ('Motor 16 CILINDROS TRUBO PLUS PLUS',23,'Motor',43234.32,'Já alguma vez foste à lua?','/interfacegrafica/Img/Motor/motor3.jpg'),
 		
-        ('Pneu 2mm rasga',4354,'Pneu',27.32,'Qualidade michelona','/interfacegrafica/Img/Motor/motor3.jpg'),
-        ('Pneu 40cm rasga estrada',3424,'Pneu',98.32,'Qualidade burnout','/interfacegrafica/Img/Motor/motor3.jpg'),
-        ('Pneu 3m Via Verde',2343,'Pneu',342.32,'Parar para quê?','/interfacegrafica/Img/Motor/motor3.jpg'),
+        ('Pneu 2mm rasga',4354,'Pneu',27.32,'Qualidade michelona','/interfacegrafica/Img/Pneus/Pneus1.jpg'),
+        ('Pneu 40cm rasga estrada',3424,'Pneu',98.32,'Qualidade burnout','/interfacegrafica/Img/Pneus/Pneus2.jpg'),
+        ('Pneu 3m Via Verde',2343,'Pneu',342.32,'Parar para quê?','/interfacegrafica/Img/Pneus/Pneus3.png'),
 		
         ('Tinta Metalica',4343,'Pintura',83.32,'Pintura manual','/interfacegrafica/Img/Motor/motor3.jpg'),
         ('Tinta Gloss',4343,'Pintura',98.32,'Pintura automatica','/interfacegrafica/Img/Motor/motor3.jpg'),
         ('Tinta Pearl',3234,'Pintura',170.32,'Pintura a jato','/interfacegrafica/Img/Motor/motor3.jpg'),
 	   
-        ('Suporte para copos',34,'Interior',98.32,'Bêba sem parar!','/interfacegrafica/Img/Motor/motor3.jpg'),
-        ('Bancos',2343,'Interior',342.32,'Sente-se no seu Trono!','/interfacegrafica/Img/Motor/motor3.jpg'),
-        ('Ar condicionado',2145, 'Interior',132.21,'Aragem fresca','/interfacegrafica/Img/Motor/motor3.jpg'),
+        ('Suporte para copos',34,'Interior',98.32,'Bêba sem parar!','/interfacegrafica/Img/Interior/SuporteParaCopos.jpg'),
+        ('Bancos',2343,'Interior',342.32,'Sente-se no seu Trono!','/interfacegrafica/Img/Interior/Bancos1.png'),
+        ('Ar condicionado',2145, 'Interior',132.21,'Aragem fresca','/interfacegrafica/Img/Interior/ArCondicionado1.jpg'),
         
-		('Retrovisor',965,'Exterior',96.32,'veja 20km para traz de si.','/interfacegrafica/Img/Motor/motor3.jpg'),
-        ('Spoiler',1746,'Exterior',174.32,'Corta tempestades','/interfacegrafica/Img/Motor/motor3.jpg'),
-        ('Teto de abrir', 0,'Exterior', 23.45,'Ver o espaço daqui','/interfacegrafica/Img/Motor/motor3.jpg'),
-        ('LEDs', 152, 'Exterior', 50.23, 'Luzes iluminantes','/interfacegrafica/Img/Motor/motor3.jpg'),
-        ('Amortecedor', 12344,'Exterior',213.21,'Para ir com speed nas lombas','/interfacegrafica/Img/Motor/motor3.jpg');
+		('Retrovisor',965,'Exterior',96.32,'veja 20km para traz de si.','/interfacegrafica/Img/Exterior/Retrovisor1.jpg'),
+        ('Spoiler',1746,'Exterior',174.32,'Corta tempestades','/interfacegrafica/Img/Exterior/Spoiler1.jpg'),
+        ('Teto de abrir', 0,'Exterior', 23.45,'Ver o espaço daqui','/interfacegrafica/Img/Exterior/Teto3.jpg'),
+        ('LEDs', 152, 'Exterior', 50.23, 'Luzes iluminantes','/interfacegrafica/Img/Exterior/Luzes2.jpg'),
+        ('Amortecedor', 12344,'Exterior',213.21,'Para ir com speed nas lombas','/interfacegrafica/Img/Exterior/Amortecedor2.jpg');
 
 INSERT INTO Modelo
 	(nome)
